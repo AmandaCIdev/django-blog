7c97bba7f1d95f8f9bd2868d308051ee847dd8fe
AmandaCIdev M <amandamascurinedev@gmail.com>
2024-02-27 15:32:24 +0000

    Add: change DEBUG value back to False ready for deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-shq-m1opq_+b)1afv$zcceh1n2^%+ie-zwm_xpq!!vxd2l!47-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '.gitpod.io', '.herokuapp.com'
