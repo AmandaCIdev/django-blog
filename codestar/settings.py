a5869e61ecb34dffb84eddc308eaa6ebf8397ace
AmandaCIdev M <amandamascurinedev@gmail.com>
2024-03-03 12:38:28 +0000

    Fix: resolve issue missing images and files from logged out issue

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,9 +29,10 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["8000-amandacidev-djangoblog-hah3elptczz.ws-eu108.gitpod.io", ".herokuapp.com"]
+ALLOWED_HOSTS = ['.gitpod.io',
+                '.herokuapp.com']
 
 
 # Application definition
